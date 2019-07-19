5124bfa0b55072e45e6b22b366060ae9a8731674
PoloChang <polo.chang@asiayo.com>
2019-07-19 22:58:34 +0800

    checkstyle check

--- server/src/main/java/com/example/Person.java
+++ server/src/main/java/com/example/Person.java
@@ -12,8 +12,10 @@ public class Person {
 
   }
 
-  //TODO: Add javadoc comment
-  public String person(String someone) {
+  /**
+   * This is a constructor.
+   */
+  public final String person(final String someone) {
     return String.format("Hello, %s!", someone);
   }
 }
